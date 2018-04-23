916dda03ad768c1bbd15ed737089d5a615ed84e5
LuisMesa <la.mesa10@uniandes.edu.co>
2018-04-22 22:40:51 -0500

    Integration with Linkedin ready, but I have been stuck with Torre Bio API like for one hour

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,34 @@ import Cover from "./Cover/Cover";
 import './App.css';
 
 class App extends Component {
+  state = {
+    authLinkedin: false,
+    authBio: false,
+    LinkedinProfile: null,
+  };
+
+  authenticateLinkedin = (param) => {
+    console.log(param);
+    this.setState({authLinkedin: true});
+    window.IN.API
+      .Profile('me')
+      .fields("id", "first-name", "last-name", "email-address", "public-profile-url", "picture-url", "picture-urls::(original)", "headline", "summary", "location", "industry", "positions")
+      .result((result)=>{console.log(result); this.setState({LinkedinProfile: result})});
+    this.exportDataFromLinkedinToBio();
+  };
+
+  authenticateBio = (param) => {
+    console.log(param);
+    this.setState({authBio: true});
+    this.exportDataFromLinkedinToBio();
+  };
+
+  exportDataFromLinkedinToBio = () => {
+    if(this.state.authLinkedin && this.state.authBio){
+
+    }
+  };
+
   render() {
     const title = <a href="htpps://bio.torre.co">
       <img src="./assets/torre-bio-logo.png" alt="Torre Bio Logo"/>
@@ -20,7 +48,11 @@ class App extends Component {
           titleStyle={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start'}}
           style={{backgroundColor: '#F5F5F5'}}
         />
-        <Cover/>
+        <Cover authLinkedin={this.state.authLinkedin}
+               authenticateLinkedin={this.authenticateLinkedin}
+               authBio={this.state.authBio}
+               authenticateBio={this.authenticateBio}
+        />
         <div className="section">
           <About/>
           <Recommendations/>
--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 import Paper from 'material-ui/Paper';
+import RaisedButton from "material-ui/RaisedButton";
 import './Cover.css';
 
 class Cover extends Component {
+  handleClickLinkedin = (event) => {
+    window.IN.User.authorize( this.props.authenticateLinkedin, window);
+  };
+  // handleClickBio = (event) => {
+  //   window.IN.User.authorize( this.props.authenticateLinkedin, window);
+  // };
   render() {
     return (
       <Paper className="Cover">
         {/*<img className="background" src="https://starrgate.s3.amazonaws.com:443/users/4037ecee8f3abf7429bde31ddcccbb67c88eae9c/profile_eKe1KUs.jpg"/>*/}
-        <img src="https://starrgate.s3.amazonaws.com:443/users/4037ecee8f3abf7429bde31ddcccbb67c88eae9c/profile_eKe1KUs.jpg" alt="User image"/>
+        {this.props.authBio && this.props.authLinkedin?
+          <img src="https://starrgate.s3.amazonaws.com:443/users/4037ecee8f3abf7429bde31ddcccbb67c88eae9c/profile_eKe1KUs.jpg" alt="User profile"/>
+          :
+          <div className="get-started">
+            <div className="title">Import all your data from Linkedin to Torre Bio now!</div>
+            <div className="description">Are you ready for the next step in your career? Welcome to Torre Bio, the new social network for professionals. To import all your data from Linkedin to Torre Bio just click the below buttons.</div>
+            <div className="actions">
+              <RaisedButton label="Linkedin" primary={true} onClick={this.handleClickLinkedin}/>
+              <RaisedButton label="Torre Bio" primary={true}/>
+            </div>
+          </div>
+        }
       </Paper>
     );
   }
